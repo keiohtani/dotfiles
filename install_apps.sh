5868584d42d6087adb4b06e1bbb5e4863e7de623
keiohtani <35276788+keiohtani@users.noreply.github.com>
2019-06-16 18:56:45 -0500

    Add installation for Dein

--- install_apps.sh
+++ install_apps.sh
@@ -6,3 +6,12 @@ brew install nvim
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# install plugin manager for NVIM
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+# For example, we just use `~/.cache/dein` as installation directory
+sh ./installer.sh ~/.cache/dein
+rm ./installer.sh
+
+# run ":call dein#install()" in NVIM
+
