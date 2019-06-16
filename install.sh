7b5b897b84efee2e480182cd4236be9f355a6841
keiohtani <35276788+keiohtani@users.noreply.github.com>
2019-06-16 18:52:07 -0500

    Clean up install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,14 @@ for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
     then
-        ln -Fis "$PWD/$dotfile" $HOME
+        ln -vis $PWD/$dotfile $HOME
     fi
 done
-ln -Fis "$PWD/.vim/colors" "$HOME/.config/nvim"
 
 for nvim_file in nvim/*
 do
-    if [ $nvim_file != '..' ]
+    if [ $nvim_file != '..' ] && [ $nvim_file != '.' ]
     then
-        ln -Fis "$PWD/$nvim_file" "$HOME/.config/nvim"
+        ln -vis $PWD/$nvim_file $HOME/.config/nvim/
     fi
 done
