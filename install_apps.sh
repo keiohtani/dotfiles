453003424fac5f3129a0cd28d96e97478c3a1888
keiohtani <35276788+keiohtani@users.noreply.github.com>
2019-06-16 22:08:48 -0500

    Update install_apps.sh for linux computers

--- install_apps.sh
+++ install_apps.sh
@@ -1,13 +1,46 @@
 #!/bin/zsh
 
-# Install editor
-echo Installing editors
-brew install nvim
+UNAME = $(uname)
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if [ UNAME = 'Darwin' ]; then
+    brew install nvim
+elif [ UNAME = 'Linux' ]; then
+    linux_setup
+fi
+
+linux_setup(){
+    
+    echo Type the username: 
+    read username
+    dependency="zsh git vim tmux uwf"
+    echo Installing $dependency
+    sudo apt-get install dependency
+    chsh -s /bin/zsh $username
+    done 
+    
+    # setup firewall using uwf
+    
+    # install fzf for tmux
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+    
+    # install nvim
+    # https://wilkins.tech/posts/neovim-raspberry-pi/
+    echo Installing nvim... 
+    sudo apt-get install cmake automake libtool libtool-bin gettext
+    done
+    git clone https://github.com/neovim/neovim.git
+    cd neovim
+    make -j8 CMAKE_BUILD_TYPE=RelWithDebInfo
+    sudo make install
+    pip3 install --user neovim
+    
+}
+
+nvim --version  # to test nvim installation
 
 # install plugin manager for NVIM
+echo Installing NVIM plugin manager
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 # For example, we just use `~/.cache/dein` as installation directory
 sh ./installer.sh ~/.cache/dein
