2b112ce1565a7948375b31cd536deadd16aeb2b1
keiohtani <35276788+keiohtani@users.noreply.github.com>
2019-08-02 13:37:08 -0400

    Add install for ubuntu

--- install_apps.sh
+++ install_apps.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 
+ubuntu_setup(){
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt-get update
+    sudo apt-get install neovim
+}
 
-linux_setup(){
+raspi_setup(){
     
     echo Type the username: 
     read username
