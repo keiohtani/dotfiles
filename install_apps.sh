2a189744ca843552636436de9bc57a44b742c1ce
keiohtani <contact@keiohtani.com>
2019-08-05 11:24:14 -0400

    Add Homebrew install for Ubuntu and fix syntax errors

--- install_apps.sh
+++ install_apps.sh
@@ -2,11 +2,14 @@
 
 basic_setup(){
 
-    username=$(user)
-    dependency="zsh git vim tmux uwf"
+    dependency="git vim tmux"
     echo Installing $dependency
     sudo apt-get install dependency
-    chsh -s /bin/zsh $username
+    sudo apt install zsh
+    chsh -s /bin/zsh $USER
+
+    zsh
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 }
 
@@ -21,8 +24,13 @@ ubuntu_setup(){
     sudo apt-get update
     sudo apt-get install neovim
 
-    zsh
-    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+    echo Installing Homebrew... 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+    brew install hello
 
 }
 
@@ -32,6 +40,7 @@ raspi_setup(){
     basic_setup
     
     # setup firewall using uwf
+    sudo apt-get install uwf
     
     # install fzf for tmux
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -41,7 +50,6 @@ raspi_setup(){
     # https://wilkins.tech/posts/neovim-raspberry-pi/
     echo Installing nvim... 
     sudo apt-get install cmake automake libtool libtool-bin gettext
-    done
     git clone https://github.com/neovim/neovim.git
     cd neovim
     make -j8 CMAKE_BUILD_TYPE=RelWithDebInfo
@@ -55,7 +63,7 @@ raspi_setup(){
     echo Enter network ID for zerotier: 
     read networkID 
     curl -s 'https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg' | gpg --import && \
-        if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash fi
+        if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
     sudo systemctl enable zerotier-one
     sudo zerotier-cli join $networkID
     echo Authenticate your device at https://my.zerotier.com/network/$networkID
@@ -63,12 +71,11 @@ raspi_setup(){
 
 }
 
+uname=$(uname)
 
-UNAME=$(uname)
-
-if [ $UNAME = 'Darwin' ]; then
+if [ $uname = 'Darwin' ]; then
     brew install nvim
-elif [ $UNAME = 'Linux' ]; then
+elif [ $uname = 'Linux' ]; then
     # linux_setup
     ubuntu_setup
 fi
@@ -83,4 +90,4 @@ sh ./installer.sh ~/.cache/dein
 rm ./installer.sh
 
 # run ":call dein#install()" in NVIM
-
+echo 'run ":call dein#install()" in neovim'
