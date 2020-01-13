70e83efb117549a5745e9305826ef9288e9d1f0b
Kei Ohtani <35276788+keiohtani@users.noreply.github.com>
2020-01-12 20:17:17 -0500

    Update basics.sh

--- .install/basics.sh
+++ .install/basics.sh
@@ -4,6 +4,6 @@ dependencies="zsh git vim tmux"
 echo Installing $dependencies
 sudo apt-get -y install $dependencies
 
-chsh -s /bin/zsh $USER
+sudo chsh -s /bin/zsh $USER
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
