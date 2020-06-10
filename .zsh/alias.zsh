637e6b6645289d5d01438e6be9aa4b1a52a9ef82
keiohtani <35276788+keiohtani@users.noreply.github.com>
2020-06-10 16:29:16 -0500

    Set up docker aliases

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -18,7 +18,13 @@ mac_setup(){
     alias raspi_camera='ssh pi@raspicamera.local -p 50022 -i ~/.ssh/id_rsa_raspi3b'
     alias raspi4nas='ssh pi@raspi4nas.local -p 50022 -i ~/.ssh/raspi4_nas'
     alias py='python'
+    docker_setup
 
+}
+
+docker_setup (){
+    alias dcc='docker-compose'
+    alias dc='docker'
 }
 
 if [[ $uname = 'Darwin' ]]; then
