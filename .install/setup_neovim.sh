72e4e05009ae4f975b3a0fb2b5476e1a72dd16ee
keiohtani <35276788+keiohtani@users.noreply.github.com>
2020-06-12 07:17:04 -0500

    Set up to install nvim dependencies

--- .install/setup_neovim.sh
+++ .install/setup_neovim.sh
@@ -1,2 +1,10 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+python3 -m pip install --user --upgrade pynvim
+
+sudo gem install neovim
+
+npm install -g neovim
+
+
