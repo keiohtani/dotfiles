ce5be12d1820f4945f0b0c0c31d2a8d73802a48f
keiohtani <35276788+keiohtani@users.noreply.github.com>
2020-08-20 14:06:33 -0400

    Set up tmux auto restore

--- install/basics.sh
+++ install/basics.sh
@@ -10,3 +10,6 @@ source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 
 # zsh plugin manager
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
