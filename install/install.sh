98a08daf7fa0f7b7646bb8fec53bf0b10ac93335
keiohtani <35276788+keiohtani@users.noreply.github.com>
2020-08-26 11:08:10 -0400

    Change to create dir if it does not exist already

--- install/install.sh
+++ install/install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 cd $(dirname $0)/..
-for dotfile in .?*
-do
-    if [[ -f $dotfile ]]
-    then
+for dotfile in .?*; do
+    if [[ -f $dotfile ]]; then
         ln -vis $PWD/$dotfile $HOME
     fi
 done
 
-for config_dir in .config/*
-do
-    if [ $config_dir != '..' ] && [ $config_dir != '.' ]
-    then
+for config_dir in .config/*; do
+    if [ -d $config_dir ]; then 
+        echo $config_dir
+        mkdir -p $HOME/$config_dir
+    fi
+    if [ $config_dir != '..' ] && [ $config_dir != '.' ]; then
         ln -vis $PWD/$config_dir/* $HOME/$config_dir
     fi
 done
