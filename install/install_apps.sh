08db33f3bef56aea981edf94a8fb79d3fd26f0e9
keiohtani <35276788+keiohtani@users.noreply.github.com>
2021-01-20 01:54:46 -0500

    Change names

--- install/install_apps.sh
+++ install/install_apps.sh
@@ -3,17 +3,16 @@
 uname=$(uname)
 
 raspi_install(){
-    sh install/basics.sh
     sudo apt-get install uwf
-    sh install/neovim_for_pi.sh
-    sh install/zerotier.sh
+    sh install/install_neovim_for_pi.sh
+    sh install/install_zerotier.sh
 }
 
 mac_install(){
     defaults write -g ApplePressAndHoldEnabled -bool false
-    sh install/node.sh
-    sh install/brew.sh
-    sh install/git.sh
+    sh install/install_node.sh
+    sh install/install_brew.sh
+    sh install/setup_git.sh
     sh vscode/install_vscode_extensions.sh 
 }
 
