8a66be455f42f504aee2bf8c1e767112956eeef6
keiohtani <35276788+keiohtani@users.noreply.github.com>
2021-04-14 16:31:54 -0400

    Hide ssh-add output

--- .zsh/github.zsh
+++ .zsh/github.zsh
@@ -2,7 +2,7 @@ uname=$(uname)
 
 if [[ $uname = 'Linux' ]]; then
 	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_rsa_github
+	ssh-add ~/.ssh/id_rsa_github &>/dev/null
 elif [[ $uname = 'Darwin' ]]; then
-	ssh-add -K ~/.ssh/id_ed25519
+	ssh-add -K ~/.ssh/id_ed25519 &>/dev/null
 fi
