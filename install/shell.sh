993a0236cbfa0f6d63d7c8a27c8b48a5068b676d
Kei Ohtani <35276788+keiohtani@users.noreply.github.com>
2024-09-04 20:19:21 -0400

    Add starship preset

--- install/shell.sh
+++ install/shell.sh
@@ -1,6 +1,9 @@
 # Install starship
 curl -sS https://starship.rs/install.sh | sh
 
+# Switch to pure
+starship preset pure-preset -o ~/.config/starship.toml
+
 # zsh plugin manager
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
